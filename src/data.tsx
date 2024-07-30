b9d0cbbe2ae93470c42891c6cf65263d0491ee6f
Maxvo <maxvo25@gmail.com>
2024-07-30 12:47:44 +0930

    add default code for challenge in data file

--- src/data.tsx
+++ src/data.tsx
@@ -1,1 +1,45 @@
+export const DEFAULT_CHALLENGE_CSS = ``;
+export const DEFAULT_CHALLENGE_JS = ``;
+export const DEFAULT_CHALLENGE_HTML = `<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Remove the line below if you don't want to use TailwindCSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<!-- Change code below this line -->
+<body class="bg-[#181028] flex justify-center items-center min-h-screen">
+    <div class="bg-white text-[#181028] p-8 space-y-4 shadow-lg rounded-xl w-full max-w-xl">
+
+        <p class="text-lg">Transform the design (bottom-right) <svg class="w-6 h-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 8h.01"></path><path d="M12 20h-5a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v5"></path><path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l4 4"></path><path d="M14 14l1 -1c.617 -.593 1.328 -.793 2.009 -.598"></path><path d="M16 19h6"></path><path d="M19 16v6"></path></svg> into a functional project using HTML and CSS.</p>
+
+        <h2 class="text-2xl">Want to go the extra mile?</h2>
+        <p class="flex gap-2 font-medium text-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="3"
+                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l5 5l10 -10"></path>
+            </svg>
+
+            Add animations and make the project interactive using JavaScript! 😎
+        </p>
+        <p class="flex gap-2 font-medium text-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="3"
+                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l5 5l10 -10"></path>
+            </svg>
+
+            Prioritize creativity over pixel-perfect accuracy and showcase your developer ingenuity.
+        </p>
+        <p class="text-sm border-t pt-6">This example uses TailwindCSS by adding the CDN in the head tag. You can import any libraries you want using a CDN.</p>
+    </div>
+</body>
+<!-- Change code above this line -->
+
+</html>`;
+
 
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -10,6 +10,7 @@ import { useEffect, useRef } from "react";
 import { useSupabase } from "../context/SupabaseContext";
 import { useToast } from "../context/ToastContext";
 import { MonacoEditor } from "../model";
+import { DEFAULT_CHALLENGE_CSS, DEFAULT_CHALLENGE_HTML, DEFAULT_CHALLENGE_JS } from "../data";
 
 interface Props {
     className?: string;
@@ -32,7 +33,7 @@ export const Admin: React.FC<Props> = ({ className }) => {
 
     useEffect(() => {
         if(isSubmitting)
-            showToast('inprogress', 'Signing in. Please wait...');
+            showToast('inprogress', 'Submitting in. Please wait...');
     }, [isSubmitting])
     
     const onSubmit = async (formData: FieldValues) => {
@@ -84,55 +85,15 @@ export const Admin: React.FC<Props> = ({ className }) => {
                         <Tab className="" tabsList={[
                             {
                                 title: 'HTML',
-                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'HTML')} theme='vs-dark' defaultLanguage="html" defaultValue={`<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Remove the line below if you don't want to use TailwindCSS -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
-</head>
-
-<!-- Change code below this line -->
-<body class="bg-[#181028] flex justify-center items-center min-h-screen">
-    <div class="bg-white text-[#181028] p-8 space-y-4 shadow-lg rounded-xl w-full max-w-xl">
-
-        <p class="text-lg">Transform the design (bottom-right) <svg class="w-6 h-6 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 8h.01"></path><path d="M12 20h-5a3 3 0 0 1 -3 -3v-10a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v5"></path><path d="M4 15l4 -4c.928 -.893 2.072 -.893 3 0l4 4"></path><path d="M14 14l1 -1c.617 -.593 1.328 -.793 2.009 -.598"></path><path d="M16 19h6"></path><path d="M19 16v6"></path></svg> into a functional project using HTML and CSS.</p>
-
-        <h2 class="text-2xl">Want to go the extra mile?</h2>
-        <p class="flex gap-2 font-medium text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="3"
-                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M5 12l5 5l10 -10"></path>
-            </svg>
-
-            Add animations and make the project interactive using JavaScript! 😎
-        </p>
-        <p class="flex gap-2 font-medium text-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" stroke-width="3"
-                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M5 12l5 5l10 -10"></path>
-            </svg>
-
-            Prioritize creativity over pixel-perfect accuracy and showcase your developer ingenuity.
-        </p>
-        <p class="text-sm border-t pt-6">This example uses TailwindCSS by adding the CDN in the head tag. You can import any libraries you want using a CDN.</p>
-    </div>
-</body>
-<!-- Change code above this line -->
-
-</html>`} />)
+                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'HTML')} theme='vs-dark' defaultLanguage="html" defaultValue={DEFAULT_CHALLENGE_HTML} />)
                             },
                             {
                                 title: 'CSS',
-                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'CSS')} theme='vs-dark' defaultLanguage="css" defaultValue={``} />)
+                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'CSS')} theme='vs-dark' defaultLanguage="css" defaultValue={DEFAULT_CHALLENGE_CSS} />)
                             },
                             {
                                 title: 'JS',
-                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'JS')} theme='vs-dark' defaultLanguage="javascript" defaultValue={``} />)
+                                content: (<Editor onMount={(editor) => handleEditorDidMount(editor, 'JS')} theme='vs-dark' defaultLanguage="javascript" defaultValue={DEFAULT_CHALLENGE_JS} />)
                             }
                         ]}></Tab>
                         <div className="flex flex-col gap-3">
